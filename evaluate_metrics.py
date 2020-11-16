babaa74e61c72129b4c60e48adec9da1eba3347a
Luca <luca.ant96@libero.it>
2020-11-16 09:11:30 +0100

    first generated canto

--- evaluate_metrics.py
+++ evaluate_metrics.py
@@ -31,8 +31,8 @@ if __name__ == '__main__':
     
     generated_canto = generated_canto.replace('CANTO', '')
 
-    print(divine_comedy)
-#    print(generated_canto)
+#    print(divine_comedy)
+    print(generated_canto)
 
 
     evaluation_results = {}
